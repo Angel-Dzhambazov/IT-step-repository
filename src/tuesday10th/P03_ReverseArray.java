47bc68667e212420d27ad4d8cfdec7ddc2ea593b
Angel Dzhambazov <angel.dzhambazov@gmail.com>
2018-04-10 10:41:15 +0300

    p03

--- src/tuesday10th/P03_ReverseArray.java
+++ src/tuesday10th/P03_ReverseArray.java
@@ -4,31 +4,34 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class P01_Array {
+public class P03_ReverseArray {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));	
 		int n = Integer.parseInt(reader.readLine());
 
+	
 		int[] array = new int[n];
 		for (int i = 0; i < array.length; i++) {
-				int digit = Integer.parseInt(reader.readLine());	
-				if (digit % 2 == 0){
-					array[i] = 0;
-				} else{
-					array[i] = digit;
-				}
-			
+			int digit = Integer.parseInt(reader.readLine());	
+				array[i] = digit;		
 		}
 		
-		for (int digit: array){
+		swapArray(array);
 		
-				System.out.print(digit+" ");		
-			
-		
+		for (int digit: array){			
+			System.out.print(digit+" ");		
 		}
 		
 		
 	}
-
+	
+	private static void swapArray(int[] array){
+		for(int i = 0; i < array.length/2; i++){
+			int digit = array[array.length-1-i];
+			array[array.length-1-i] = array[i];
+			array[i] = digit;
+		}
+		
+	}
 }
